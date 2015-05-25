bd780728a20bd0496ee60d155e639b42b5a2bfc3
upa8 <pratikupacharya@gmail.com>
2015-05-26 00:52:41 +0530

    Selection Sort Added

--- src/All_Sorts.java
+++ src/All_Sorts.java
@@ -1,20 +1,24 @@
 
 public class All_Sorts {
 	public static void main(String[] args){
-
-		int a[] = {1,2,3,4,5,6,7,8,9};
+		// Worst case
+		int a[] = {9,8,7,6,5,4,3,2,1};
+		// Best Case
+	   //int a[] = {1,2,3,4,5,6,7,8,9};
+		
 		for(int i = 0 ; i<a.length ; i++){
 			System.out.print(a[i]);
 			System.out.print(" ");
 		}
 		System.out.println("\n");
 		// Bubble Sort 
-				Bubble_Sort(a);
-				System.out.println("\n");
+				//Bubble_Sort(a);
+				//System.out.println("\n");
 		// Improved Bubble Sort 
-				Improved_Bubble_Sort(a);
+				//Improved_Bubble_Sort(a);
 		//Selection Sort 
-				
+				Selection_Sort(a);
+		
 
 		
 		
@@ -24,13 +28,26 @@ public class All_Sorts {
 			System.out.print(" ");
 	
 		}
-		
 	
+	}
 
+	private static void Selection_Sort(int[] a) {
+		// TODO Auto-generated method stub
+		int count_comparisions=0 ;
 		
-		
-		
-		
+		for(int i = 0 ; i < a.length - 1; i ++){
+			int min = i;
+			for(int j = i + 1 ; j < a.length ; j++){
+				if(a[j]<a[min]){
+					min = j;
+				}
+				count_comparisions++;
+			}
+			int temp = a[i];
+			a[i]     = a[min];
+			a[min]=temp;
+		}
+		System.out.println(count_comparisions);
 	}
 
 	public static void Improved_Bubble_Sort(int[] m) {
