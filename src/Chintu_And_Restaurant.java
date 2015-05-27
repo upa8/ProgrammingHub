2ee3ab8bc8f7ee4cf16d51907d00955de21f2c18
upa8 <pratikupacharya@gmail.com>
2015-05-27 16:25:30 +0530

    Joined Hackerearth and solved few problems on hackerearth

---
+++ src/Chintu_And_Restaurant.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class Chintu_And_Restaurant {
+	public static void main(String args[]){
+		Scanner Input = new Scanner(System.in);
+		String S = Input.next();
+		int N = Input.nextInt();
+		for(int k =0 ; k< N; k ++){
+			int i = Input.nextInt();
+			int j = Input.nextInt();
+			if(S.charAt(i-1) == S.charAt(j-1)){
+				System.out.println("Yes");
+			}else{
+				System.out.println("No");
+				
+			}
+		}
+		
+	}
+
+}
