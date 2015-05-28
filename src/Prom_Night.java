f2ea2256985e7328337ecf8fe9c04505e3a723e8
upa8 <pratikupacharya@gmail.com>
2015-05-29 01:15:40 +0530

    New try for prom night

--- src/Prom_Night.java
+++ src/Prom_Night.java
@@ -20,42 +20,26 @@ public class Prom_Night {
 			if(y>=x && (x<10000) && (y< 10000)){
 				String MALE = br.readLine();
 				String FEMALE = br.readLine();
-				// add all the values into an array for male
-				String[] Male_Hieghts = MALE.split(" ");
-				
+				String[] Male_Hieghts = MALE.split(" ");	
 				ArrayList<Integer> Male_Array = new ArrayList<Integer>(); 
-				
-			//	int[] Male_Array = new int[10000];
 				int m = 0;
 				for (String token : Male_Hieghts){
-				//	Male_Array[m++] = Integer.parseInt(token); 
 					Male_Array.add(Integer.parseInt(token));
 				}
-				//Similar for female
 				String[] FeMale_Hieghts = FEMALE.split(" ");
 				ArrayList<Integer> FeMale_Array = new ArrayList<Integer>(); 
-				
-				//int[] FeMale_Array = new int[10000];
-
 				int k = 0;
 				for (String tokens : FeMale_Hieghts){
-			//		FeMale_Array[k++] = Integer.parseInt(tokens); 
 					FeMale_Array.add(Integer.parseInt(tokens));
 					
 				}
 				Collections.sort(Male_Array);
 				Collections.sort(FeMale_Array);
-				//Arrays.sort(Male_Array);
-				//Arrays.sort(FeMale_Array);
-				int s = Male_Array.size() ;
-	//			System.out.println(Male_Array.get(s-1)+ " " + FeMale_Array.get(s-1));
-				
-		
+				int s = Male_Array.size() ;	
 				if(Male_Array.get(s-1)>=FeMale_Array.get(s-1)){
 					System.out.println("YES");
 				}else{
 					System.out.println("NO");
-					
 				}
 				
 			}else{
