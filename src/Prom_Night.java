6e40c8035f1346e74b836e432cd91769f4b07b32
upa8 <pratikupacharya@gmail.com>
2015-05-30 10:59:11 +0530

    new

--- src/Prom_Night.java
+++ src/Prom_Night.java
@@ -20,26 +20,59 @@ public class Prom_Night {
 			if(y>=x && (x<10000) && (y< 10000)){
 				String MALE = br.readLine();
 				String FEMALE = br.readLine();
-				String[] Male_Hieghts = MALE.split(" ");	
-				ArrayList<Integer> Male_Array = new ArrayList<Integer>(); 
-				int m = 0;
-				for (String token : Male_Hieghts){
-					Male_Array.add(Integer.parseInt(token));
-				}
+				String[] Male_Hieghts = MALE.split(" ");
 				String[] FeMale_Hieghts = FEMALE.split(" ");
-				ArrayList<Integer> FeMale_Array = new ArrayList<Integer>(); 
-				int k = 0;
-				for (String tokens : FeMale_Hieghts){
-					FeMale_Array.add(Integer.parseInt(tokens));
+				int m_size = Male_Hieghts.length;
+				int f_size = FeMale_Hieghts.length;
+//				System.out.println(m_size + " " + f_size);
+				
+				if((m_size==x)&&(f_size==y)){
+					ArrayList<Integer> Male_Array = new ArrayList<Integer>(); 
+					int m = 0;
+					for (String token : Male_Hieghts){
+						Male_Array.add(Integer.parseInt(token));
+					}
+					ArrayList<Integer> FeMale_Array = new ArrayList<Integer>(); 
+					int k = 0;
+					for (String tokens : FeMale_Hieghts){
+						FeMale_Array.add(Integer.parseInt(tokens));
+						
+					}
+					Collections.sort(Male_Array);
+					Collections.sort(FeMale_Array);
+				//	System.out.println(Male_Array);
+				//	System.out.println(FeMale_Array);
+					int s = Male_Array.size() ;	
+				//	System.out.println(Male_Array.get(3)+" "+ s);
+
+				//	System.out.println(FeMale_Array.get(s-1));
 					
-				}
-				Collections.sort(Male_Array);
-				Collections.sort(FeMale_Array);
-				int s = Male_Array.size() ;	
-				if(Male_Array.get(s-1)>=FeMale_Array.get(s-1)){
-					System.out.println("YES");
+					boolean flag = true;
+					for(int w = 0 ; w< s; w++){
+						if(Male_Array.get(w) >= FeMale_Array.get(w)){
+						//	System.out.println(Male_Array.get(w) + " C" + FeMale_Array.get(w));
+						//	Male_Array.remove(w);
+							
+						//	FeMale_Array.remove(w);
+						}else{
+							flag = false;
+							break;
+						}
+					}
+				
+					if(flag){
+						System.out.println("YES");
+						
+					}else{
+						System.out.println("NO");
+						
+					}
+					
+
+					
 				}else{
 					System.out.println("NO");
+					
 				}
 				
 			}else{
--- src/testing.java
+++ src/testing.java
@@ -1,32 +1,23 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
-import java.util.StringTokenizer;
 
 
 public class testing {
 	public static void main(String args[]) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BigInteger n,a,b,s,result,d  ;//= BigInteger.ONE;
+		BigInteger n,k,b,s,result,d  ;//= BigInteger.ONE;
 		s = BigInteger.ZERO;
 		d = BigInteger.ONE;
 		String str = br.readLine();
-		n = new BigInteger(str);
-		boolean flag = true;
-		for (BigInteger bi = n;
-	            bi.compareTo(BigInteger.ZERO) > 0;
-	            bi = bi.subtract(BigInteger.ONE)) {
+		String[] numbers = str.split(" ");
+		n = new BigInteger(numbers[0]);
+		k = new BigInteger(numbers[1]);
+		for (BigInteger bi = n; bi.compareTo(BigInteger.ZERO) > 0; bi = bi.subtract(BigInteger.ONE)){
 			String strs = br.readLine();
-		  	String[] numbers = strs.split(" ");
-		      
-		//	StringTokenizer st = new StringTokenizer(strs);
-	//		String first = //	st.nextToken();
-	//		String second = //	st.nextToken();
-			a = new BigInteger(numbers[0]);
+		  	b =   new BigInteger(strs);
+			
+		  	a = new BigInteger(numbers[0]);
 			b = new BigInteger(numbers[1]);
 			result = a.add(b);
 			System.out.println(result);
@@ -39,4 +30,4 @@ public class testing {
 		
 	
 	}
-}		
+}
