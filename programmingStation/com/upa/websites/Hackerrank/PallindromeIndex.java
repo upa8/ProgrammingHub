c54335a2fb6d7e8a56b9104ff4996a68f5f2b7cb
upa8 <pratikupacharya@gmail.com>
2015-12-30 14:31:58 +0530

    Pallindrome Index Solution added

---
+++ programmingStation/com/upa/websites/Hackerrank/PallindromeIndex.java
@@ -0,0 +1,41 @@
+package com.upa.websites.Hackerrank;
+
+public class PallindromeIndex extends MyScanner {
+
+	public static void main(String args[]) throws Exception {
+		PallindromeIndex in = new PallindromeIndex();
+		int n = in.nextInt();
+		while (n > 0) {
+			String str = in.nextToken();
+			int l = str.length();
+			int m = l - 1;
+			boolean pallindrome = true;
+			for (int i = 0; i < (l / 2); i++) {
+				// System.out.println(str.charAt(m));
+				// System.out.println(str.charAt(i));
+				// System.out.println(str.charAt(i + 1));
+				// System.out.println(str.charAt(m - 1));
+
+				if (str.charAt(m) == str.charAt(i)) {
+					m--;
+				} else {
+					if (str.charAt(i + 1) == str.charAt(m)) {
+						System.out.println(i);
+						pallindrome = false;
+						break;
+					} else {
+						if (str.charAt(i) == str.charAt(m - 1)) {
+							System.out.println(m);
+							pallindrome = false;
+							break;
+						}
+					}
+				}
+			}
+			if (pallindrome) {
+				System.out.println(-1);
+			}
+			n--;
+		}
+	}
+}
---
+++ programmingStationTest/com/upa/websites/hackerrankTest/PallindromeIndexTest.java
@@ -0,0 +1,22 @@
+package com.upa.websites.hackerrankTest;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.upa.websites.Hackerrank.PallindromeIndex;
+
+public class PallindromeIndexTest {
+
+	@BeforeClass
+	public void BeforeClass() {
+		PallindromeIndex test = new PallindromeIndex();
+	}
+
+	@Test
+	public void test() {
+
+		Assert.assertTrue(true);
+	}
+
+}
