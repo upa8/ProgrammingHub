2d628b3793d677f3c7b914b2e0b2adc06d721867
upa8 <pratikupacharya@gmail.com>
2016-01-04 21:08:01 +0530

    NewYearParty.java added

---
+++ programmingStation/com/upa/websites/Hackerrank/NewYearParty.java
@@ -0,0 +1,31 @@
+package com.upa.websites.Hackerrank;
+
+import java.util.Arrays;
+
+public class NewYearParty extends MyScanner {
+
+	public static void main(String args[]) throws Exception {
+		NewYearParty in = new NewYearParty();
+		int n = in.nextInt();
+		int t[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			t[i] = Integer.parseInt(in.nextToken());
+		}
+		Arrays.sort(t);
+		long time = t[0];
+		for (int i = 1; i < n; i++) {
+			if (t[i] == t[i - 1]) {
+				time = time + 1;
+			} else {
+				if (time < t[i]) {
+					time = t[i];
+				} else {
+					time++;
+				}
+			}
+		}
+		System.out.println(time);
+
+	}
+
+}
