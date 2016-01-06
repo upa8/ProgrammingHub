4a36420a32d86c566869b59fc5933b2cf3978d6c
sprc1333 <saarthakpande@gmail.com>
2016-01-07 00:54:20 +0530

    Reverse a list

---
+++ programmingStation/com/upa/websites/Hackerrank/ResversaList.java
@@ -0,0 +1,28 @@
+/*
+ * Author : Saarthak Pandey
+ * Question : https://www.hackerrank.com/challenges/reverse-a-linked-list
+ */
+/*
+  Reverse a linked list and return pointer to the head
+  The input list will have at least one element  
+  Node is defined as  
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+    // This is a "method-only" submission. 
+    // You only need to complete this method. 
+Node Reverse(Node head) {
+    Node prev=null,cur,nex;
+    cur=head;
+    while(cur!=null)
+        {
+        nex     = cur.next;
+        cur.next= prev;
+        prev    = cur;
+        cur     = nex;
+    }
+    return prev;
+
+}
