a232bcedcbbc185cce0bdbe7cc4acf77563966ec
upa8 <pratikupacharya@gmail.com>
2016-02-24 20:02:05 +0530

    CoinChange problem solved

---
+++ programmingStation/com/upa/websites/HackerEarth/MonkAndSomeQueries.java
@@ -0,0 +1,10 @@
+package com.upa.websites.hackerEarth;
+
+import com.upa.templates.MyScanner;
+
+public class MonkAndSomeQueries extends MyScanner {
+
+	public static void main(String args[]) {
+
+	}
+}
---
+++ programmingStation/com/upa/websites/Hackerrank/CoinChangeProblem.java
@@ -0,0 +1,37 @@
+package com.upa.websites.hackerrank;
+
+import com.upa.templates.MyScanner;
+
+public class CoinChangeProblem extends MyScanner {
+
+	public static void main(String args[]) throws Exception {
+
+		CoinChangeProblem in = new CoinChangeProblem();
+		int n = in.nextInt();
+		int m = in.nextInt();
+		int coins[] = new int[m];
+		for (int i = 0; i < coins.length; i++) {
+			coins[i] = in.nextInt();
+		}
+
+		System.out.println(in.numberOfSolutions(n, coins));
+	}
+
+	public long numberOfSolutions(int total, int coins[]) {
+		long temp[][] = new long[coins.length + 1][total + 1];
+		for (int i = 0; i <= coins.length; i++) {
+			temp[i][0] = 1;
+		}
+		for (int i = 1; i <= coins.length; i++) {
+			for (int j = 1; j <= total; j++) {
+				if (coins[i - 1] > j) {
+					temp[i][j] = temp[i - 1][j];
+				} else {
+					temp[i][j] = temp[i][j - coins[i - 1]] + temp[i - 1][j];
+				}
+			}
+		}
+		return temp[coins.length][total];
+	}
+
+}
