68ce463d9e9e9b6472c0f15be7cfdae4efe1c3b7
upa8 <pratikupacharya@gmail.com>
2016-08-28 22:16:50 +0530

    Solved 389 problem

---
+++ programmingStation/com/upa/websites/LeetCode/P389FindDifference.java
@@ -0,0 +1,26 @@
+package com.upa.websites.leetCode;
+
+import com.upa.templates.MyScanner;
+
+public class P389FindDifference extends MyScanner {
+	public static void main(String args[]) throws Exception {
+		P389FindDifference in = new P389FindDifference();
+		String s = in.nextToken();
+		String t = in.nextToken();
+		System.out.println(in.findTheDifference(s, t));
+	}
+
+	public char findTheDifference(String s, String t) {
+		int length = s.length();
+		int sumS = 0;
+		int sumT = 0;
+		for (int i = 0; i < length; i++) {
+			sumS += (s.charAt(i) - 'a');
+			sumT += (t.charAt(i) - 'a');
+		}
+		sumT += t.charAt(length);
+		System.out.println(sumT - sumS);
+
+		return (char) (sumT - sumS);
+	}
+}
